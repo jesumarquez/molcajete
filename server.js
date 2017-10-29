d9d568d4c1b1ad3135cdd8d8cfc992882a12506c
Jesú Márquez <jesumarquez@gmail.com>
2017-10-29 16:45:59 -0300

    about

--- server.js
+++ server.js
@@ -8,8 +8,12 @@ var express = require('express'),
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
 app.get('/', function(req, res){
     res.render('index');
+});
+app.get('/about', function(req, res){
+    res.render('about');
 });
 
 app.listen(port, function(){
