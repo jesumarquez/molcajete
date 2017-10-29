e5619d6379743272b8f5407bfc52312fb2ae3738
Jesú Márquez <jesumarquez@gmail.com>
2017-10-29 17:15:52 -0300

    bootrstrap

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var express = require('express'),
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'node_modules')));
 
 app.get('/', function(req, res){
     res.render('index');
