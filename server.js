12acc4328341bfd7a5e47c2662a5f03100c81aba
Jesú Márquez <jesumarquez@gmail.com>
2017-10-29 16:37:32 -0300

    pug template view

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require('express'),
     app = express(),
+    path = require('path'),
     bodyParser = require('body-parser');
     config = require('./config'),
     port = process.env.PORT || config.app.port;
 
-app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res){
+    res.render('index');
+});
 
 app.listen(port, function(){
     console.log('server is running...');
