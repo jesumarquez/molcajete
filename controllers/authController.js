fcba4e4aa58792359d5964f3a2b293411d8fc922
Jesú Márquez <jesumarquez@gmail.com>
2017-10-31 14:13:21 -0300

    about con autorizacion

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,4 +11,13 @@ authController.doLogin = function(req, res){
     });
 } 
 
+authController.isAuthenticated = function () {
+  return function (req, res, next) {
+      if(req.isAuthenticated()){
+          return next();
+      }
+      res.redirect('/');
+  }  
+};
+
 module.exports = authController;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express         = require('express'),
     app             = express.Router(),
     homeControler   = require('../controllers/homeController');
+    authControler   = require('../controllers/authController');
 
 app.get('', homeControler.home);
 
-app.get('/about', homeControler.about);
+app.get('/about', authControler.isAuthenticated(), homeControler.about);
 
 module.exports = app;
